f96d3ec32832ce6d8f9acd0bf86be3e210c5b881
BrioAsh <128207348+BrioAsh@users.noreply.github.com>
2025-06-04 16:05:12 +0200

    Initial commit

---
+++ Tableau d'aide.js
@@ -0,0 +1,10 @@
+// Commentaire sous une seule ligne
+
+/*
+Commentaire sous
+plusieurs ligne
+*/
+
+// La variable [console] nous permets de gérer tout ce qui est réalisable depuis la console [clear, log, debug, info, time, etc]
+console.log("Ce message serre à laisser un message dans la console.")
+
---
+++ script.js
@@ -0,0 +1,31 @@
+// Ceci est un test de script pour "Site_test.html"
+console.log("Le cacaaaaa");
+
+
+// Les let : Pour une variable dont la valeur peut changer.
+let nom = "Alice";
+console.log(nom); // Affiche "Alice"
+nom = "Bob";
+console.log(nom); // Affiche "Bob"
+
+
+// Les const : Pour une constante, une valeur qui ne changera jamais.
+const PI = 3.14159
+console.log(PI); // Affiche 3.14159
+// PI = 3.14; // Ceci provoquerait une erreur car on ne peut pas modifier une constante
+
+
+// Les String : Du texte, toujours entre guillemets ([""] ou ['']).
+let message = "Bonjour tout le monde !";
+let prenom = 'Sophie';
+
+
+// Les Number : Des nombres, entiers ou décimaux.
+let age = 30;
+let prix = 19.99
+
+
+// Les Boolean : Représente vrai ou faux ([true] ou [false]).
+let estConnecte = true
+let aTermine = false
+
